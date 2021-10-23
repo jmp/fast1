9bc90303ffce27a75422b259fa458d2ae8fc2192
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-23 16:32:37 +0300

    Remove unnecessary isort excludes
    These are already skipped thanks to .gitignore.

--- lint.sh
+++ lint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -e
 
+echo "Checking code with black..."
 black --check .
+
+echo "Checking imports with isort..."
 isort --check .
+
+echo "Checking code with flake8..."
 flake8 .
+
+echo "Checking types with mypy..."
 mypy --strict .
