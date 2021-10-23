28909bd859d328b763db33f8993ed9e021a9629e
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-23 17:09:57 +0300

    Use Poetry to run linters

--- lint.sh
+++ lint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 
 echo "Checking code with black..."
-black --check .
+poetry run black --check .
 
 echo "Checking imports with isort..."
-isort --check .
+poetry run isort --check .
 
 echo "Checking code with flake8..."
-flake8 .
+poetry run flake8 .
 
 echo "Checking types with mypy..."
-mypy --strict .
+poetry run mypy --strict .
