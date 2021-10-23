584af856e98dec4fb390f9decaf9ff5e5374f5dd
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-23 19:30:37 +0300

    Remove root endpoint

--- app/main.py
+++ app/main.py
@@ -4,8 +4,3 @@ from .api import health
 
 app = FastAPI()
 app.include_router(health.router)
-
-
-@app.get("/")
-def read_root() -> dict[str, str]:
-    return {"msg": "Hello World"}
--- tests/test_main.py
+++
@@ -1,7 +0,0 @@
-from fastapi.testclient import TestClient
-
-
-def test_read_root(client: TestClient) -> None:
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
