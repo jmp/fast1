e6ec7dec8546717aa6417d60aca6f690d1299275
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-23 19:51:17 +0300

    Remove database check from health endpoint

--- app/api/health.py
+++ app/api/health.py
@@ -1,12 +1,8 @@
 from fastapi import APIRouter
 
-from ..db.engine import engine
-from ..health import check_health
-
 router = APIRouter()
 
 
 @router.get("/health")
-async def health() -> dict[str, str]:
-    check_health(engine)
-    return {"status": "OK"}
+async def health() -> None:
+    pass
