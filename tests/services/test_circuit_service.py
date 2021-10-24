ce3aada2521132323efbac111afd3cfee26834ff
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-24 17:17:45 +0300

    Remove reference to use case

--- tests/services/test_circuit_service.py
+++ tests/services/test_circuit_service.py
@@ -1,21 +1,20 @@
 from typing import Optional
 
 from app.domain.circuit import Circuit
-from app.ports.api.get_circuit_use_case import GetCircuitUseCase
 from app.ports.spi.get_circuit_port import GetCircuitPort
 from app.services.circuit_service import CircuitService
 
 
 def test_get_circuit_returns_circuit_if_it_exists(monza: Circuit) -> None:
     adapter = _create_get_circuit_adapter(monza)
-    service: GetCircuitUseCase = CircuitService(adapter)
+    service = CircuitService(adapter)
     circuit = service.get_circuit(monza.ref)
     assert circuit == monza
 
 
 def test_get_circuit_returns_none_if_circuit_does_not_exist() -> None:
     adapter = _create_get_circuit_adapter(None)
-    service: GetCircuitUseCase = CircuitService(adapter)
+    service = CircuitService(adapter)
     circuit = service.get_circuit("does_not_exist")
     assert circuit is None
 
