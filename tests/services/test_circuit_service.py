1088dab68ce1ac3ce43841ebca17ef5f6e1d594a
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-24 17:15:48 +0300

    Simplify circuit service tests

--- tests/services/test_circuit_service.py
+++ tests/services/test_circuit_service.py
@@ -7,20 +7,22 @@ from app.services.circuit_service import CircuitService
 
 
 def test_get_circuit_returns_circuit_if_it_exists(monza: Circuit) -> None:
-    class GetCircuitAdapter(GetCircuitPort):
-        def get_circuit(self, ref: str) -> Optional[Circuit]:
-            return monza
-
-    service: GetCircuitUseCase = CircuitService(GetCircuitAdapter())
+    adapter = _create_get_circuit_adapter(monza)
+    service: GetCircuitUseCase = CircuitService(adapter)
     circuit = service.get_circuit("monza")
     assert circuit == monza
 
 
 def test_get_circuit_returns_none_if_circuit_does_not_exist() -> None:
-    class GetCircuitAdapter(GetCircuitPort):
-        def get_circuit(self, ref: str) -> Optional[Circuit]:
-            return None
-
-    service: GetCircuitUseCase = CircuitService(GetCircuitAdapter())
+    adapter = _create_get_circuit_adapter(None)
+    service: GetCircuitUseCase = CircuitService(adapter)
     circuit = service.get_circuit("does_not_exist")
     assert circuit is None
+
+
+def _create_get_circuit_adapter(circuit: Optional[Circuit]) -> GetCircuitPort:
+    class GetCircuitAdapter(GetCircuitPort):
+        def get_circuit(self, ref: str) -> Optional[Circuit]:
+            return circuit
+
+    return GetCircuitAdapter()
