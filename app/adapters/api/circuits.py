23338f8dd7f75bfc678e1510c76259a41e6438cd
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-24 13:15:37 +0300

    Raise 404 if circuit is not found

--- app/adapters/api/circuits.py
+++ app/adapters/api/circuits.py
@@ -1,19 +1,21 @@
 from typing import Any
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
 
 
 @router.get("/circuits/{ref}")
 async def circuits(ref: str) -> dict[str, Any]:
-    return {
-        "ref": "monza",
-        "name": "Autodromo Nazionale di Monza",
-        "location": "Monza",
-        "country": "Italy",
-        "latitude": 45.6156,
-        "longitude": 9.28111,
-        "altitude": 162,
-        "url": "http://en.wikipedia.org/wiki/Autodromo_Nazionale_Monza",
-    }
+    if ref == "monza":
+        return {
+            "ref": "monza",
+            "name": "Autodromo Nazionale di Monza",
+            "location": "Monza",
+            "country": "Italy",
+            "latitude": 45.6156,
+            "longitude": 9.28111,
+            "altitude": 162,
+            "url": "http://en.wikipedia.org/wiki/Autodromo_Nazionale_Monza",
+        }
+    raise HTTPException(404)
