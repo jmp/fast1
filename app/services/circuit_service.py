4c360cdf6ef90eddf2cd798f6ed8f076c701c50f
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-24 14:37:07 +0300

    Rename attr to adapter

--- app/services/circuit_service.py
+++ app/services/circuit_service.py
@@ -6,8 +6,8 @@ from app.ports.spi.get_circuit_port import GetCircuitPort
 
 
 class CircuitService(GetCircuitUseCase):
-    def __init__(self, get_circuit: GetCircuitPort):
-        self.get_circuit_port = get_circuit
+    def __init__(self, get_circuit_adapter: GetCircuitPort):
+        self._get_circuit_adapter = get_circuit_adapter
 
     def get_circuit(self, ref: str) -> Optional[Circuit]:
-        return self.get_circuit_port.get_circuit(ref)
+        return self._get_circuit_adapter.get_circuit(ref)
