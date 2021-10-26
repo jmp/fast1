fdc223caa23550db8baf1bf2ccdab76ed8a41fdf
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-26 21:08:17 +0300

    Remove Protocol from use case

--- app/ports/api/get_circuit_use_case.py
+++ app/ports/api/get_circuit_use_case.py
@@ -1,9 +1,8 @@
-from asyncio import Protocol
 from typing import Optional
 
 from app.domain.circuit import Circuit
 
 
-class GetCircuitUseCase(Protocol):
+class GetCircuitUseCase:
     def get_circuit(self, ref: str) -> Optional[Circuit]:
         raise NotImplementedError
