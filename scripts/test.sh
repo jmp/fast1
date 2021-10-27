ea474b8867021f06ecc8e2d94af212137b0a05a6
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-27 19:18:30 +0300

    Add some echoes for debugging

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
 if [ -n "$CI" ]; then
+  echo "Running tests with coverage..."
   poetry run pytest --cov=. --cov-report=xml
 else
+  echo "Running tests without coverage..."
   poetry run pytest
 fi
