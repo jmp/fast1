32aeb4a90d98b84370f9ad17d759085540ca37d4
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-27 20:21:51 +0300

    Rename builder function

--- tests/services/test_circuit_service.py
+++ tests/services/test_circuit_service.py
@@ -7,20 +7,20 @@ from tests.domain.circuits import monza
 
 
 def test_get_circuit_returns_circuit_if_it_exists() -> None:
-    adapter = _create_get_circuit_adapter(monza)
+    adapter = _build_get_circuit_adapter(monza)
     service = CircuitService(adapter)
     circuit = service.get_circuit(monza.ref)
     assert circuit == monza
 
 
 def test_get_circuit_returns_none_if_circuit_does_not_exist() -> None:
-    adapter = _create_get_circuit_adapter(None)
+    adapter = _build_get_circuit_adapter(None)
     service = CircuitService(adapter)
     circuit = service.get_circuit("does_not_exist")
     assert circuit is None
 
 
-def _create_get_circuit_adapter(circuit: Optional[Circuit]) -> GetCircuitPort:
+def _build_get_circuit_adapter(circuit: Optional[Circuit]) -> GetCircuitPort:
     class GetCircuitAdapter(GetCircuitPort):
         def get_circuit(self, ref: str) -> Optional[Circuit]:
             return circuit
