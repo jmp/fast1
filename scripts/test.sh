f29df4ad81ec550b117900dad9b69607e4020a1d
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-30 23:51:22 +0300

    Make test script more robust

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,22 @@
 #!/bin/sh -e
 
 if [ -n "$CI" ]; then
-  echo "Running tests with coverage..."
-  docker compose up db -d
-  while ! mysqladmin ping -h 127.0.0.1 -u db --password=db 2> /dev/null; do
+  stopdb=
+  if ! docker compose ps --filter 'status=running' --services | grep -q db; then
+    echo "Starting database..."
+    docker compose up db -d
+    stopdb=1
+  fi
+  echo "Waiting for database to be up..."
+  while ! mysqladmin ping -h 127.0.0.1 -udb -pdb 2> /dev/null; do
     sleep 0.1
   done
+  echo "Running tests with coverage..."
   poetry run pytest --cov=. --cov-report=xml
+  if [ -n "$stopdb" ]; then
+    echo "Stopping database..."
+    docker compose down db
+  fi
 else
   echo "Running tests without coverage..."
   poetry run pytest
