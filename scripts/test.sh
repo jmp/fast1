6848e115425e6d137b088dfc53c5d316d6031a3d
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-10-30 21:44:39 +0300

    Wait until DB is available

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,9 @@
 if [ -n "$CI" ]; then
   echo "Running tests with coverage..."
   docker compose up db -d
-  sleep 10
+  while ! mysqladmin ping -h 127.0.0.1 -u db --password=db 2> /dev/null; do
+    sleep 0.1
+  done
   poetry run pytest --cov=. --cov-report=xml
 else
   echo "Running tests without coverage..."
